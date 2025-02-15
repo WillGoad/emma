a2412bec3a9438878eec9a5c150f08a20cef2d7a
Will Goad <will@goad.id>
2025-02-15 22:00:20 +0000

    Frontend docker file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,7 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ['logo.clearbit.com', 'www.coinjar.com', 'www.cryptomkt.com'], // Add the required domain here
+    domains: ["logo.clearbit.com", "www.coinjar.com", "www.cryptomkt.com"], // Add the required domain here
   },
-    async rewrites() {
-      return process.env.NODE_ENV === 'development' ? [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-        }
-      ] : [];
-    }
-  };
+  output: "standalone",
+};
