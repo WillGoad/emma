9ee9e9cf8280a46155d9df5b1899ac501801a212
Will Goad <will@goad.id>
2025-02-15 23:47:09 +0000

    Testing envs

--- api/src/services/scheduler.ts
+++ api/src/services/scheduler.ts
@@ -23,7 +23,7 @@ export function startScheduler() {
       console.log(`Starting task: ${task.name}`);
       try {
         await task.job();
-        console.log(`Completed task: ${task.name}`);
+        console.log(`Completed task: ${task.name} ${process.env.FRONTEND_URL}`);
       } catch (error) {
         console.error(`Error in task ${task.name}:`, error);
       }
